b6283531b12e8b208bd3e1a80049be77a2d667d8
Luis Filipe Sales <luisfilipels@hotmail.com>
2021-04-05 21:40:18 -0300

    Implemented follow-up 1 of part 3.

--- src/Assignment1/Part3.java
+++ src/Assignment1/Part3.java
@@ -6,20 +6,32 @@ public class Part3 {
 
     static class MyStack {
         private int[] stackArray = new int[8];
+        private int[] minStackArray = new int[8];
         private int top = 0;
 
-        private int[] resize() {
+        private int[][] resize() {
             int len = stackArray.length;
             int [] newArray = new int[len * 2];
+            int [] newMin = new int[len * 2];
 
-            System.arraycopy(stackArray, 0, newArray, 0, stackArray.length);
+            System.arraycopy(stackArray, 0, newArray, 0, len);
+            System.arraycopy(minStackArray, 0, newMin, 0, len);
 
-            return newArray;
+            return new int[][] {
+                    newArray, newMin
+            };
         }
 
         public void push(int x) {
-            if (top == stackArray.length) stackArray = resize();
-            stackArray[top++] = x;
+            if (top == stackArray.length) {
+                int[][] res = resize();
+                stackArray = res[0];
+                minStackArray = res[1];
+            }
+            stackArray[top] = x;
+            if (top != 0) minStackArray[top] = Math.min(x, minStackArray[top-1]);
+            else minStackArray[top] = x;
+            top++;
         }
 
         public int pop() {
@@ -30,6 +42,11 @@ public class Part3 {
         public int top() {
             if (isEmpty()) throw new EmptyStackException();
             return stackArray[top-1];
+        }
+
+        public int min() {
+            if (isEmpty()) throw new EmptyStackException();
+            return minStackArray[top-1];
         }
 
         public int size() {
@@ -47,6 +64,7 @@ public class Part3 {
 
         System.out.println(stack.isEmpty());
         stack.push(42);
+        System.out.println(stack.min());
 
         System.out.println(stack.isEmpty());
         System.out.println(stack.top());
@@ -57,10 +75,22 @@ public class Part3 {
         }
 
         while (!stack.isEmpty()) {
+            System.out.println(stack.min());
             System.out.println(stack.pop());
         }
 
+        stack.push(5);
+        stack.push(4);
+        stack.push(6);
+
+        System.out.println(stack.min());
+
+        stack.push(3);
+        System.out.println(stack.min());
         stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.min());
     }
 
 }
