f3dcaa0062bfdd02d1f50cfcc5020eed218c4822
Luis Filipe Sales <luisfilipels@hotmail.com>
2021-04-05 21:54:02 -0300

    Implemented the second follow-up to Part 3

--- src/Assignment1/Part3.java
+++ src/Assignment1/Part3.java
@@ -4,47 +4,61 @@ import java.util.EmptyStackException;
 
 public class Part3 {
 
-    static class MyStack {
-        private int[] stackArray = new int[8];
-        private int[] minStackArray = new int[8];
+    static abstract class MyStack<T> {
+        private T[] stackArray;
+        private T[] minStackArray;
         private int top = 0;
 
-        private int[][] resize() {
+        MyStack() {
+            stackArray = (T[]) new Object[8];
+            minStackArray = (T[]) new Object[8];
+        }
+
+        private T[][] resize() {
             int len = stackArray.length;
-            int [] newArray = new int[len * 2];
-            int [] newMin = new int[len * 2];
+            T [] newArray = (T[]) new Object[len * 2];
+            T [] newMin = (T[]) new Object[len * 2];
 
             System.arraycopy(stackArray, 0, newArray, 0, len);
             System.arraycopy(minStackArray, 0, newMin, 0, len);
 
-            return new int[][] {
+            return (T[][]) new Object[][] {
                     newArray, newMin
             };
         }
 
-        public void push(int x) {
+        abstract int compare(T t1, T t2);
+
+        public void push(T x) {
             if (top == stackArray.length) {
-                int[][] res = resize();
+                T[][] res = resize();
                 stackArray = res[0];
                 minStackArray = res[1];
             }
             stackArray[top] = x;
-            if (top != 0) minStackArray[top] = Math.min(x, minStackArray[top-1]);
+            if (top != 0) {
+                int comparison = compare(x, minStackArray[top-1]);
+                if (comparison < 0) {
+                    minStackArray[top] = x;
+                } else {
+                    minStackArray[top] = minStackArray[top-1];
+                }
+            }
             else minStackArray[top] = x;
             top++;
         }
 
-        public int pop() {
+        public T pop() {
             if (top == 0) throw new EmptyStackException();
             return stackArray[--top];
         }
 
-        public int top() {
+        public T top() {
             if (isEmpty()) throw new EmptyStackException();
             return stackArray[top-1];
         }
 
-        public int min() {
+        public T min() {
             if (isEmpty()) throw new EmptyStackException();
             return minStackArray[top-1];
         }
@@ -59,7 +73,13 @@ public class Part3 {
     }
 
     public static void main(String[] args) {
-        MyStack stack = new MyStack();
+        System.out.println("Testing stack of integers: ");
+        MyStack<Integer> stack = new MyStack<>() {
+            @Override
+            int compare(Integer t1, Integer t2) {
+                return t1 - t2;
+            }
+        };
 
 
         System.out.println(stack.isEmpty());
@@ -91,6 +111,21 @@ public class Part3 {
         stack.pop();
         stack.pop();
         System.out.println(stack.min());
+
+        System.out.println("Testing stack of strings");
+        MyStack<String> stack2 = new MyStack<String>() {
+            @Override
+            int compare(String t1, String t2) {
+                return t1.length() - t2.length();
+            }
+        };
+
+        stack2.push("abc");
+        stack2.push("abcd");
+        stack2.push("ab");
+        System.out.println(stack2.min());
+        stack2.pop();
+        System.out.println(stack2.min());
     }
 
 }
