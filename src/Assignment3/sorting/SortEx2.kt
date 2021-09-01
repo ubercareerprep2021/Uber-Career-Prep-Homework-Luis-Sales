710669e8cd5f18c41956bc80464ecc0f3750465b
Luis Filipe <luisfilipels@hotmail.com>
2021-09-01 14:02:09 -0300

    Added comments and test cases to SortEx2

--- src/Assignment3/sorting/SortEx2.kt
+++ src/Assignment3/sorting/SortEx2.kt
@@ -5,18 +5,12 @@ import kotlin.collections.ArrayList
 
 class SortEx2 {
 
-    /*
-    for (i in 0..arr.size) {
-            for (j in arr.indices) {
-                Arrays.sort(arr, j, min(j + memSize, arr.lastIndex))
-            }
-        }
-    */
-
+    // This class executes the Merge part of the MergeSort algorithm. Given a ramArray (the integers in RAM),
+    // and fileArray (the integers stored in the hard disk), it merges both arrays.
     class ArrayMergeHandler constructor(private val ramArray: IntArray, private val fileArray: IntArray) {
 
-        private var rPointer = 0
-        private var fPointer = 0
+        private var rPointer = 0 // Pointer to the current position in ramArray
+        private var fPointer = 0 // Pointer to the current position in fileArray
 
         private fun nextInRam() : Int{
             return ramArray[rPointer++]
@@ -34,6 +28,7 @@ class SortEx2 {
             }
         }
 
+        // Standard merge algorithm
         fun merge() : IntArray {
             val helperList = ArrayList<Int>()
             while (rPointer < ramArray.size && fPointer < fileArray.size)
@@ -54,21 +49,19 @@ class SortEx2 {
 
     }
 
-    fun getChunkFromTo(array: IntArray, start: Int, end: Int) : IntArray {
-        val returnArray = IntArray(end - start)
-        var k = start
-        for (j in 0 until (end - start)) {
-            returnArray[j] = array[k++]
-        }
-        return returnArray
-    }
+    // This function simulates an external mergesort algorithm. It takes in fullArray, that is the array
+    // to be sorted (and can be considered to be a file), and memSize, the maximum amount of elements we can have
+    // in the RAM at any given moment.
+    fun externalSort(fullArray: IntArray, memSize: Int = 100) : IntArray {
 
-    fun externalSort(fullArray: IntArray, memSize: Int) : IntArray {
+        // chunkCount is the amount of ranges of size memSize that we can have in fullArray
         val chunkCount = if (fullArray.size % memSize == 0) fullArray.size / memSize else (fullArray.size/memSize) + 1
+
+        // outFile is considered to be the external file to which we are storing the sorted fullArray
         var outFile = IntArray(0)
 
         for (i in 0 until chunkCount) {
-            val currentChunk = getChunkFromTo(fullArray, i * memSize, (i * memSize) + memSize)
+            val currentChunk = fullArray.copyOfRange(i * memSize, (i * memSize) + memSize)
             Arrays.sort(currentChunk)
             outFile = ArrayMergeHandler(currentChunk, outFile).merge()
         }
@@ -77,33 +70,4 @@ class SortEx2 {
     }
 
 
-}
-
-fun testWithRandomArray(arraySize: Int, memorySize: Int) {
-
-    val testArray = IntArray(arraySize)
-    val random = Random()
-
-    for (i in 0 until arraySize) {
-        testArray[i] = random.nextInt(arraySize)
-    }
-
-    val compareArray = testArray.clone()
-    compareArray.sort()
-
-    val sort = SortEx2()
-    val sortedArray = sort.externalSort(testArray, memorySize)
-
-    for (i in 0 until arraySize) {
-        if (sortedArray[i] != compareArray[i]) {
-            println("NOT SORTED!")
-            return
-        }
-    }
-
-    println("SORTED!")
-}
-
-fun main() {
-    testWithRandomArray(10000, 100)
 }
