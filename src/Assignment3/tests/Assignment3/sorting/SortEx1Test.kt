9d80db1d7fc0bccaf257bc8cd7ccef816b301e26
Luis Filipe <luisfilipels@hotmail.com>
2021-09-01 12:45:03 -0300

    Improved test cases of SortEx1

--- src/Assignment3/tests/Assignment3/sorting/SortEx1Test.kt
+++ src/Assignment3/tests/Assignment3/sorting/SortEx1Test.kt
@@ -9,28 +9,104 @@ internal class SortEx1Test {
     private val sort = SortEx1()
 
     @Test
-    fun swap() {
-
+    fun testSwapEqualValues() {
+        val arr = intArrayOf(0, 0, 1)
+        sort.swap(arr, 0, 1)
+        assertArrayEquals(intArrayOf(0, 0, 1), arr)
     }
 
     @Test
-    fun threePartitionSort() {
-        val testCases = arrayOf(
-            intArrayOf(5, 10, 20, 5, 10, 20),
-            intArrayOf(20, 5, 20, 20, 5, 10, 20, 20, 5, 10, 20),
-            intArrayOf(5, 5, 5, 10, 10, 10, 20, 20, 20),
-            intArrayOf(5),
-            intArrayOf(10),
-            intArrayOf(20),
-            intArrayOf(5, 20),
-            intArrayOf(10, 5)
-        )
+    fun testSwapCase1() {
+        val arr = intArrayOf(1, 0, 1)
+        sort.swap(arr, 0, 1)
+        assertArrayEquals(intArrayOf(0, 1, 1), arr)
+    }
 
-        for (testCase in testCases) {
-            val expectedValue = testCase.clone()
-            expectedValue.sort()
-            sort.threePartitionSort(testCase)
-            assertArrayEquals(expectedValue, testCase)
-        }
+    @Test
+    fun testSwapCase2() {
+        val arr = intArrayOf(0, 1, 1)
+        sort.swap(arr, 0, 1)
+        assertArrayEquals(intArrayOf(1, 0, 1), arr)
+    }
+
+    @Test
+    fun testSortCase1 () {
+        val testCase = intArrayOf(5, 10, 20, 5, 10, 20)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase2 () {
+        val testCase = intArrayOf(20, 5, 20, 20, 5, 10, 20, 20, 5, 10, 20)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase3 () {
+        val testCase = intArrayOf(5, 5, 5, 10, 10, 10, 20, 20, 20)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase4 () {
+        val testCase = intArrayOf(5)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase5 () {
+        val testCase = intArrayOf(10)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase6 () {
+        val testCase = intArrayOf(20)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase7 () {
+        val testCase = intArrayOf(5, 20)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase8 () {
+        val testCase = intArrayOf(10, 5)
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
+    }
+
+    @Test
+    fun testSortCase9 () {
+        val testCase = intArrayOf()
+        val expectedValue = testCase.clone()
+        expectedValue.sort()
+        sort.threePartitionSort(testCase)
+        assertArrayEquals(expectedValue, testCase)
     }
 }
