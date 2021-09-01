7516e1fb4408149b4d3b042878d320e18281c2f5
Luis Filipe <luisfilipels@hotmail.com>
2021-09-01 15:54:35 -0300

    Added comments and test cases to SortEx5

--- src/Assignment3/sorting/SortEx5.kt
+++ src/Assignment3/sorting/SortEx5.kt
@@ -8,21 +8,16 @@ class SortEx5 {
         array[j] = temp
     }
 
+    // This function takes in an intArray, and organizes it in such a way that for every index i,
+    // arr[i-1] <= arr[i] >= arr[i+1] or arr[i-1] >= arr[i] <= arr[i+1].
     fun peaksAndValleys(array: IntArray){
+        // By sorting the array, for every index i, we have that arr[i] <= arr[i+1] <= ...
         array.sort()
 
+        // After this, by swapping every even index, we will have that arr[0] >= arr[1] <= arr[2] and so on.
         for (i in 0 until array.lastIndex step 2) {
             swap(array, i, i+1)
         }
     }
 
-}
-
-fun main() {
-    val sort = SortEx5()
-
-    val array = intArrayOf(3, 1, 5, 2)
-    sort.peaksAndValleys(array)
-
-    println(array.contentToString())
 }
