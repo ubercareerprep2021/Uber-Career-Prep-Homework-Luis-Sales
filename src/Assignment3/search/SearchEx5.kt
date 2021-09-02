bc8f8259cf7907c1670a4a9d61d90fe57e4f9b19
Luis Filipe <luisfilipels@hotmail.com>
2021-09-01 21:09:57 -0300

    Added a comment to exercise 5

--- src/Assignment3/search/SearchEx5.kt
+++ src/Assignment3/search/SearchEx5.kt
@@ -2,6 +2,8 @@ package Assignment3.search
 
 class SearchEx5 {
 
+    // This function searches for a value in a 2D matrix that is sorted (both row-wise, and column-wise)
+    // in O(n+m), with n being the height, and m being the width of the matrix.
     fun searchValueInMatrix(matrix: Array<Array<Int>>, target: Int) : Boolean {
         var currentRow = 0
         var currentCol = matrix.lastIndex
